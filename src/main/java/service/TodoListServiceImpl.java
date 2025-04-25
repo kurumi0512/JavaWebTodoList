41d016b4b95c9a6ebe0c8bf2318c72f027285def
kurumi0512 <foreverlove0512t@gmail.com>
2025-04-25 15:19:10 +0800

    250425 15:19

--- src/main/java/service/TodoListServiceImpl.java
+++ src/main/java/service/TodoListServiceImpl.java
@@ -21,39 +21,46 @@ public class TodoListServiceImpl implements TodoListService {
 
 	@Override
 	public List<TodoDTO> findAllTodos() {
-		return dao.findAllTodos().stream().map(this::transferToDTO) // 把每個元素轉換成dto
+		return dao.findAllTodos().stream().map(this::transferToDTO) // 把每個元素轉換成dto // 把 Entity → DTO
 				// .map(todo -> transferToDTO(todo))
 				.toList();
 	}
 
-	// 轉換成dTO
+	// 把從資料庫抓到的 Entity（Todo）轉成前端能用的 DTO（TodoDTO）
 	@Override
 	public TodoDTO getTodo(Integer id) {
 		return transferToDTO(dao.getTodo(id));
 	}
 
 	@Override
+	// 因為要新增到資料庫所以直接用todo
 	public void addTodo(String text, Boolean completed) {
-		Todo todo = new Todo(0, text, completed);
-		dao.addTodo(todo);
+		Todo todo = new Todo(0, text, completed); // 先包成 Entity
+		dao.addTodo(todo); // 存進資料庫
 	}
 
+	// 不需要 DTO，直接處理資料庫就好
+	// 因為已經知道id,就不用包成 Entity(不用new一個todo物件)
 	@Override
 	public void updateTodoComplete(Integer id, Boolean completed) {
 		dao.updateTodoComplete(id, completed);
 	}
 
+	// 不需要 DTO，直接處理資料庫就好
+	// 因為已經知道id,就不用包成 Entity(不用new一個todo物件)
 	@Override
 	public void updateTodoText(Integer id, String text) {
 		dao.updateTodoText(id, text);
 	}
 
+	// 不需要 DTO，直接處理資料庫就好
+	// 因為已經知道id,就不用包成 Entity(不用new一個todo物件)
 	@Override
 	public void deleteTodo(Integer id) {
 		dao.deleteTodo(id);
 	}
 
-	// 轉換成 DTO
+	// 轉換成 DTO的方法,這樣不用一直寫
 	private TodoDTO transferToDTO(Todo todo) {
 		return new TodoDTO(todo.getId(), todo.getText(), todo.getCompleted());
 	}
